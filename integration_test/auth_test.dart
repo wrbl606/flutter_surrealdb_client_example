fce8dd88b020b6ab4b290fee317e0860a1c4973b
Marcin Wróblewski <nicram606@gmail.com>
2022-09-29 22:20:52 +0200

    Add tests

---
+++ integration_test/auth_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+import 'package:surreal_auth/main.dart';
+
+void main() {
+  late final int timestamp;
+  const counterClicks = 10;
+  setUpAll(() {
+    timestamp = DateTime.now().millisecondsSinceEpoch;
+  });
+
+  patrolTest('signs up user', ($) async {
+    await $.pumpWidgetAndSettle(const MyApp());
+    await $(TextField).first.enterText('user$timestamp');
+    await $(TextField).last.enterText('password$timestamp');
+    await $('Sign up').tap();
+    expect($('You have pushed the button this many times:'), findsOneWidget);
+
+    for (var i = 0; i < counterClicks; i++) {
+      await $(FloatingActionButton).tap();
+    }
+
+    expect($(counterClicks.toRadixString(10)), findsOneWidget);
+  });
+
+  patrolTest('signs in user', ($) async {
+    await $.pumpWidgetAndSettle(const MyApp());
+    await $(TextField).first.enterText('user$timestamp');
+    await $(TextField).last.enterText('password$timestamp');
+    await $('Sign in').tap();
+    expect($('You have pushed the button this many times:'), findsOneWidget);
+
+    expect($(counterClicks.toRadixString(10)), findsOneWidget);
+
+    for (var i = 0; i < counterClicks; i++) {
+      await $(FloatingActionButton).tap();
+    }
+
+    expect($((counterClicks * 2).toRadixString(10)), findsOneWidget);
+  });
+}
